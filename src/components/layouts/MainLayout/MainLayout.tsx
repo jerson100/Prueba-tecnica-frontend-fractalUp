b55afe1c5fdbd18da6afd20cb4616db17a8d5564
jerson100 <juamkoo@gmail.com>
2023-03-31 13:18:40 -0500

    Add dependencies

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
+import { Outlet } from "react-router-dom";
 import Header from "../../commons/Header";
 import NavApp from "../../commons/NavApp";
 import { ContentStyle, MainLayoutContainerStyle } from "./mainLayout.style";
-import { Outlet } from "react-router-dom";
 import MusicPlayer from "../../commons/MusicPlayer";
 import { SearchProvider } from "../../../contexts/SearchContext";
 import { PlayerMusicProvider } from "../../../contexts/PlayerMusicContext";
