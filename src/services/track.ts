a11d2089ab1160f0bbeadb0abb434550b7a217f6
jerson100 <juamkoo@gmail.com>
2023-04-04 00:30:26 -0500

    replace url free deezer

--- src/services/track.ts
+++ src/services/track.ts
@@ -1,6 +1,6 @@
 import { Track } from "../styled";
 
-const api = "https://api.deezer.com/search/track";
+const api = "https://deezerdevs-deezer.p.rapidapi.com/search";
 
 export const getTracks = async ({
   track,
@@ -11,13 +11,14 @@ export const getTracks = async ({
 }): Promise<Track[]> => {
   const tracks = await fetch(
     `
-    https://cors-anywhere.herokuapp.com/${api}?q=${track}&limit=11`,
+    ${api}?q=${track}&limit=11`,
     {
       method: "GET",
       signal,
       headers: {
         Accept: "application/json",
         "Access-Control-Allow-Origin": "*",
+        "X-RapidAPI-Key": "b6b3b0de06mshc612d246812aff2p1fae78jsn37dfe72ee593",
       },
     }
   );
