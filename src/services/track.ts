0f1b0f9152bbd0fd354ecd6623a9153bd7d3d9db
jerson100 <juamkoo@gmail.com>
2023-04-04 00:35:55 -0500

    free api

--- src/services/track.ts
+++ src/services/track.ts
@@ -17,7 +17,6 @@ export const getTracks = async ({
       signal,
       headers: {
         Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
         "X-RapidAPI-Key": "b6b3b0de06mshc612d246812aff2p1fae78jsn37dfe72ee593",
       },
     }
